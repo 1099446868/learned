264b638888215c4ea9d14fb9a09baafcf86ebbdb
1099446868 <1099446868@qq.com>
2021-09-26 15:21:34 +0800

    test

---
+++ src/Learned/Test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2021/9/26
+ * Time: 13:21
+ */
+
+namespace Learned;
+
+
+class Test {
+
+    function test()
+    {
+        echo "Form Test -  > test()";
+    }
+}
