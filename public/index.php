9f0006c97f0bab300296d7c4255ef6b806cc3aca
1099446868 <1099446868@qq.com>
2021-09-26 15:20:44 +0800

    test

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2021/9/26
+ * Time: 13:20
+ */
+
+require '../vendor/autoload.php';
+
+$Test = new Learned\Test();
+$Test->test();
